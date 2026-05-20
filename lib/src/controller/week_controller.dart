5c9c52f9c80a3cef2c3a19622ab2dd38d9ca0723
9hokage4 <claymamedov206@gmail.com>
2026-05-20 19:16:43 +0300

    Рефокторинг под отслеживание для PageView

--- lib/src/controller/week_controller.dart
+++ lib/src/controller/week_controller.dart
@@ -4,21 +4,41 @@ import '../model/week_calculator.dart';
 
 class WeekController extends ChangeNotifier {
   final WeekCalculator _calculator = WeekCalculator();
-  late Week _currentWeek;
 
-  WeekController() {
-    _currentWeek = _calculator.getCurrentWeek();
+  // Индекс смещения: 0 = текущая неделя, -1 = прошлая, +1 = следующая
+  int _weekOffset = 0;
+  Week _baseWeek; // реальная текущая неделя
+
+  WeekController() : _baseWeek = WeekCalculator().getCurrentWeek();
+
+  int get weekOffset => _weekOffset;
+
+  Week get currentWeek {
+    if (_weekOffset == 0) return _baseWeek;
+    return Week(_baseWeek.startMonday.add(Duration(days: 7 * _weekOffset)));
   }
 
-  Week get currentWeek => _currentWeek;
+  void setWeekOffset(int offset) {
+    if (_weekOffset != offset) {
+      _weekOffset = offset;
+      notifyListeners();
+    }
+  }
 
   void previousWeek() {
-    _currentWeek = _calculator.getPreviousWeek(_currentWeek);
+    _weekOffset--;
     notifyListeners();
   }
 
   void nextWeek() {
-    _currentWeek = _calculator.getNextWeek(_currentWeek);
+    _weekOffset++;
+    notifyListeners();
+  }
+
+  // Сброс к текущей неделе (если нужно)
+  void resetToCurrent() {
+    _baseWeek = _calculator.getCurrentWeek();
+    _weekOffset = 0;
     notifyListeners();
   }
 }
