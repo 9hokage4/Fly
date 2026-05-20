1bc7e69163b52d0dc63f37930abececdf9376ea2
9hokage4 <claymamedov206@gmail.com>
2026-05-20 18:22:14 +0300

    Первоначальная настройка yaml

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppConstants {
+  static const Color primaryColor = Colors.blue;
+  static const Color accentColor = Colors.blueAccent;
+  static const Color backgroundColor = Colors.white;
+  static const Color cardColor = Color(0xFFF5F5F5);
+  static const double defaultPadding = 16.0;
+  static const double smallPadding = 8.0;
+  static const double borderRadius = 12.0;
+  static const Duration animationDuration = Duration(milliseconds: 300);
+}
